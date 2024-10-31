9f215047d6a5486b6a1d28f81b4b5860820942ec
20Mathieu03 <mpvmathieu03@gmail.com>
2024-10-31 17:52:40 -0400

    Initialize insertFromCSV

--- peer-assessment-app/app/Instructor/page.tsx
+++ peer-assessment-app/app/Instructor/page.tsx
@@ -8,6 +8,7 @@ import CourseCard from "../components/Course"
 import StudentList from "../components/StudentList";
 import LeftSidebar from "../components/LeftSidebar";
 import { Typography } from "@mui/material";
+import InsertCSV from "../components/InsertCSV";
 
 export default function Home() {
 
@@ -28,13 +29,13 @@ export default function Home() {
 
         {/* This is a template of the course dashboard to be implemented later */}
         <LeftSidebar />
-
         {course?
         <Box sx={{display: "flex", flexWrap: "wrap", marginLeft: "100px", padding: "10px", gap:"7px"}}>
             <Box sx={{width: "100%", marginBottom: "20px"}}>
                 <Typography variant="h4">
                     Hi Ms. Joumana!
                 </Typography>
+                <InsertCSV />
                 {/* <Divider sx={{bgcolor: "black", border: "1px solid black", width: "400px", margin: ""}}/> */}
             </Box>
 
---
+++ peer-assessment-app/app/components/InsertCSV.tsx
@@ -0,0 +1,9 @@
+
+export default function InsertCSV() {
+
+    function handleOnClick () {
+
+    }
+
+    return <a href="#" onClick={handleOnClick}>Insert list of students from file</a>
+}
