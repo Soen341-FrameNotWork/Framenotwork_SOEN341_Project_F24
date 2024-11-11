c29f3dda645f730667034f7f96e7087de6d35a6a
airi <sakutaidf@gmail.com>
2024-11-10 20:37:47 -0500

    fixed lil bug

--- peer-assessment-app/app/(dashboard)/Instructor/components/ResultSummary.tsx
+++ peer-assessment-app/app/(dashboard)/Instructor/components/ResultSummary.tsx
@@ -7,7 +7,7 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { TableVirtuoso, TableComponents } from 'react-virtuoso';
-import { Button, IconButton} from '@mui/material';
+import { IconButton } from '@mui/material';
 import DownloadIcon from '@mui/icons-material/Download';
 
 interface Data {
