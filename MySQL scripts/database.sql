f5f9ab2912a6d9a71e2708ddd0538413791495f6
M-a-a-d-man <evanteboul@icloud.com>
2024-11-14 14:54:29 -0500

    uncommented sql code

--- MySQL scripts/database.sql
+++ MySQL scripts/database.sql
@@ -1,88 +1,86 @@
--- DROP DATABASE IF EXISTS defaultdb;
+DROP DATABASE IF EXISTS defaultdb;
+CREATE DATABASE IF NOT EXISTS defaultdb;
+USE defaultdb;
 
--- CREATE DATABASE IF NOT EXISTS defaultdb;
-
--- USE defaultdb;
-
--- CREATE TABLE IF NOT EXISTS instructors (
--- 	i_id INT AUTO_INCREMENT PRIMARY KEY,
---     i_name VARCHAR(50) NOT NULL,
---     i_email VARCHAR(50) UNIQUE NOT NULL,
---     i_password VARCHAR(60) NOT NULL
--- );
+CREATE TABLE IF NOT EXISTS instructors (
+	i_id INT AUTO_INCREMENT PRIMARY KEY,
+    i_name VARCHAR(50) NOT NULL,
+    i_email VARCHAR(50) UNIQUE NOT NULL,
+    i_password VARCHAR(60) NOT NULL
+);
 
 
 
--- CREATE TABLE IF NOT EXISTS students (
---     s_id INT AUTO_INCREMENT PRIMARY KEY,
---     s_name VARCHAR(50) NOT NULL,
---     s_email VARCHAR(50) UNIQUE NOT NULL,
---     s_password VARCHAR(60) NOT NULL
--- );
+CREATE TABLE IF NOT EXISTS students (
+    s_id INT AUTO_INCREMENT PRIMARY KEY,
+    s_name VARCHAR(50) NOT NULL,
+    s_email VARCHAR(50) UNIQUE NOT NULL,
+    s_password VARCHAR(60) NOT NULL
+);
 
 
 
--- CREATE TABLE IF NOT EXISTS courses (
---     c_id INT AUTO_INCREMENT PRIMARY KEY,
---     c_name VARCHAR(50) UNIQUE NOT NULL,
---     instructor_id INT NOT NULL,
---     FOREIGN KEY (instructor_id) REFERENCES instructors(i_id)
--- );
+CREATE TABLE IF NOT EXISTS courses (
+    c_id INT AUTO_INCREMENT PRIMARY KEY,
+    c_name VARCHAR(50) UNIQUE NOT NULL,
+    instructor_id INT NOT NULL,
+    FOREIGN KEY (instructor_id) REFERENCES instructors(i_id)
+);
 
 
--- CREATE TABLE IF NOT EXISTS teams (
---     t_id INT AUTO_INCREMENT PRIMARY KEY,
---     t_name VARCHAR(50) NOT NULL,
---     course_id INT NOT NULL,
---     FOREIGN KEY (course_id) REFERENCES courses(c_id),
---     UNIQUE(t_name, course_id)
--- );
+CREATE TABLE IF NOT EXISTS teams (
+    t_id INT AUTO_INCREMENT PRIMARY KEY,
+    t_name VARCHAR(50) NOT NULL,
+    course_id INT NOT NULL,
+    FOREIGN KEY (course_id) REFERENCES courses(c_id),
+    UNIQUE(t_name, course_id)
+);
 
 
 
--- CREATE TABLE IF NOT EXISTS team_student (
---     team_id INT NOT NULL,
---     student_id INT NOT NULL,
---     course_id INT NOT NULL,
---     FOREIGN KEY (team_id) REFERENCES teams(t_id),
---     FOREIGN KEY (student_id) REFERENCES students(s_id),
---     FOREIGN KEY (course_id) REFERENCES courses(c_id),
---     UNIQUE(student_id, course_id),
---     PRIMARY KEY(team_id, student_id, course_id)
--- );
+CREATE TABLE IF NOT EXISTS team_student (
+    team_id INT NOT NULL,
+    student_id INT NOT NULL,
+    course_id INT NOT NULL,
+    FOREIGN KEY (team_id) REFERENCES teams(t_id),
+    FOREIGN KEY (student_id) REFERENCES students(s_id),
+    FOREIGN KEY (course_id) REFERENCES courses(c_id),
+    UNIQUE(student_id, course_id),
+    PRIMARY KEY(team_id, student_id, course_id)
+);
 
--- CREATE TABLE IF NOT EXISTS course_student (
---     course_id INT NOT NULL,
---     student_id INT NOT NULL,
---     FOREIGN KEY (course_id) REFERENCES courses(c_id),
---     FOREIGN KEY (student_id) REFERENCES students(s_id),
---     primary key(course_id, student_id)
--- );
+CREATE TABLE IF NOT EXISTS course_student (
+    course_id INT NOT NULL,
+    student_id INT NOT NULL,
+    FOREIGN KEY (course_id) REFERENCES courses(c_id),
+    FOREIGN KEY (student_id) REFERENCES students(s_id),
+    primary key(course_id, student_id)
+);
 
 
--- CREATE TABLE IF NOT EXISTS ratings (
---     r_id INT AUTO_INCREMENT ,
---     reviewer_id INT NOT NULL,
---     reviewee_id INT NOT NULL,
---     cooperative_score INT NOT NULL CHECK (cooperative_score >=1 AND cooperative_score <= 5),
---     cooperative_comment MEDIUMTEXT,
---     conceptual_score INT NOT NULL CHECK (conceptual_score >=1 AND conceptual_score <= 5),
---     conceptual_comment MEDIUMTEXT,
---     practical_score INT NOT NULL CHECK (practical_score >=1 AND practical_score <= 5),
---     practical_comment MEDIUMTEXT,
---     work_ethic_score INT NOT NULL CHECK (work_ethic_score >=1 AND work_ethic_score <= 5),
---     work_ethic_comment MEDIUMTEXT,
---     team_id INT NOT NULL,
---     overall_score Decimal(2,1) NOT NULL CHECK (overall_score >=1 AND overall_score <= 5),
---     FOREIGN KEY (reviewer_id) REFERENCES students(s_id),
---     FOREIGN KEY (reviewee_id) REFERENCES students(s_id),
---     FOREIGN KEY (team_id) REFERENCES teams(t_id),
---     UNIQUE (reviewer_id, reviewee_id, team_id),
---     PRIMARY KEY(r_id),
---     CHECK (reviewer_id != reviewee_id)
--- );
+CREATE TABLE IF NOT EXISTS ratings (
+    r_id INT AUTO_INCREMENT ,
+    reviewer_id INT NOT NULL,
+    reviewee_id INT NOT NULL,
+    cooperative_score INT NOT NULL CHECK (cooperative_score >=1 AND cooperative_score <= 5),
+    cooperative_comment MEDIUMTEXT,
+    conceptual_score INT NOT NULL CHECK (conceptual_score >=1 AND conceptual_score <= 5),
+    conceptual_comment MEDIUMTEXT,
+    practical_score INT NOT NULL CHECK (practical_score >=1 AND practical_score <= 5),
+    practical_comment MEDIUMTEXT,
+    work_ethic_score INT NOT NULL CHECK (work_ethic_score >=1 AND work_ethic_score <= 5),
+    work_ethic_comment MEDIUMTEXT,
+    team_id INT NOT NULL,
+    overall_score Decimal(2,1) NOT NULL CHECK (overall_score >=1 AND overall_score <= 5),
+    FOREIGN KEY (reviewer_id) REFERENCES students(s_id),
+    FOREIGN KEY (reviewee_id) REFERENCES students(s_id),
+    FOREIGN KEY (team_id) REFERENCES teams(t_id),
+    UNIQUE (reviewer_id, reviewee_id, team_id),
+    PRIMARY KEY(r_id),
+    CHECK (reviewer_id != reviewee_id)
+);
 
--- select * from courses;
+select * from courses;
 
 
 
